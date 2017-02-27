445afe45351a86d3253e32293a393a1a4da5af36
Manu Tamminen <mavata@gmail.com>
2017-02-27 09:47:58 +0100

    Tweaks

--- epic_barcoder/__init__.py
+++ epic_barcoder/__init__.py
@@ -1,1 +1,1 @@
-from .epic_barcoder import BCSeq, process_barcode_info, add_otus_to_fasta, run_array_job, make_split_dict
+from .epic_barcoder import BCSeq, process_barcode_info, add_otus_to_fasta, run_array_job, run_batch_job, make_split_dict
